34f84960d1334bb1763016899233bcad0a2bfaef
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2026-05-05 16:21:37 +0800

    feat(Layout.tsx): Prevent page refresh while navigating
    - Added the Outlet component

---
+++ src/Layout.tsx
@@ -0,0 +1,17 @@
+import { Navbar } from "@/pages/navbar"
+import { Outlet } from "react-router-dom"
+import { Footer } from "@/pages/footer"
+
+export function Layout() {
+  return (
+    <div>
+      <Navbar />
+      {/* TODO: Add contact us section at the top "Need assistance? Call us now!" */}
+      <main className="container mx-auto grow p-4">
+        {/* Reference for outlet component: https://stackoverflow.com/a/73573109/14841168 */}
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
+}

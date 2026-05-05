c47f9b62744cb67e1431048fce04ec72ba6196d6
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2026-05-05 17:27:04 +0800

    feat: Add dedicated page component
    - A visually appealing service detail page when clicking services card

---
+++ src/components/layout/page.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom"
+import type { Service } from "@/types"
+
+interface ServiceDetailPageProps {
+  serviceCardDetails: Service
+}
+
+export function ServiceDetailPage({
+  serviceCardDetails,
+}: ServiceDetailPageProps) {
+  const buttonText = "Book This Service"
+  return (
+    <div>
+      <h1 className="scroll-m-20 text-center text-2xl font-extrabold tracking-tight text-balance">
+        {serviceCardDetails.name}
+      </h1>
+      <blockquote className="mt-6 border-l-2 pl-4 italic">
+        {serviceCardDetails.description}
+      </blockquote>
+      <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+        {serviceCardDetails.details.map((detail) => (
+          <li key={detail}>{detail}</li>
+        ))}
+      </ul>
+      <div className="grid place-content-center">
+        <Button>
+          <Link to="/contact" state={{ serviceName: serviceCardDetails.title }}>
+            {buttonText}
+          </Link>
+        </Button>
+      </div>
+    </div>
+  )
+}

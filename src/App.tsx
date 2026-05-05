f60efb35d155c879e3b8fd55c1c86c4c56e51bc4
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2026-05-05 17:20:15 +0800

    feat(App,main): Use Routes inside of App.tsx
    - Moved BrowserRouter into main.tsx
    - Cleanup App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,16 @@
-import { BrowserRouter, Link } from "react-router-dom"
-import { HamburgerMenu } from "@/components/layout/hamburger"
-import { services } from "@/data/services"
-import { ServiceCard } from "@/components/layout/card"
-
-function Services() {
-  return (
-    <div>
-      <h2 className="mb-12 text-center text-3xl font-bold">Our Services</h2>
-      <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
-        {services.map((service) => (
-          <Link
-            to={`/services/${service.id}`}
-            key={service.id}
-            className="block overflow-hidden rounded-lg bg-white shadow-md transition-shadow duration-300 hover:shadow-xl"
-          >
-            <div id={service.id}>
-              <ServiceCard serviceDetails={service} />
-            </div>
-          </Link>
-        ))}
-      </div>
-    </div>
-  )
-}
+import { Layout } from "@/Layout"
+import { Routes, Route } from "react-router-dom"
+import { Home } from "@/pages/home"
+import { ServiceDetail } from "@/pages/service-details"
 
 export function App() {
   return (
-    <BrowserRouter>
-      <div className="sticky top-0 z-50 flex justify-between bg-background/80 backdrop-blur">
-        <Link to={"/"}>
-          <h1 className="p-4 text-2xl font-bold">MB AUTO</h1>
-        </Link>
-        <div className="p-4">
-          <HamburgerMenu />
-        </div>
-      </div>
-      <Services />
-    </BrowserRouter>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/services/:serviceId" element={<ServiceDetail />} />
+      </Route>
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
-import App from "./App.tsx"
+import { ThemeProvider } from "@/components/theme-provider"
+import App from "./App"
+import { BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>
 )
